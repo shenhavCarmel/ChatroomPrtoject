bcb832dac01e9c6d911ed290c241596263f4d83f
unknown <shenhavc@post.bgu.ac.il>
2018-04-03 21:00:55 +0300

    progress with GUI class + User.cs

--- MileStone1/LogicLayer/User.cs
+++ MileStone1/LogicLayer/User.cs
@@ -5,9 +5,39 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace MileStone1.LogicLayer
-
 {
     class User
     {
+
+        private Boolean isLoggedIn;
+        private String nickname;
+        private int groupId;
+
+        public User (String nickname, int groupId)
+        {
+            this.isLoggedIn = false;
+            this.nickname = nickname;
+            this.groupId = groupId;
+        }
+
+        public void logout()
+        {
+            isLoggedIn = false;
+        }
+
+        public void login()
+        {
+            isLoggedIn = true;
+        }
+
+        public Message sendMessage(string msg)
+        {
+            Message message= CommunicationLayer.Communication.send(ChatRoom.URL, this.groupId, msg);
+        }
+        
+        private void saveToPresistent()
+        {
+
+        }
     }
 }
--- MileStone1/PresentationLayer/GUI.cs
+++ MileStone1/PresentationLayer/GUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MileStone1.LogicLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +9,11 @@ namespace MileStone1.PresentationLayer
 {
     class GUI
     {
-        string _startMenuBar= "~Group25 ChatRoom~" + "\r\n" + "1- Register" + "\r\n" + "2- Login" + 
+        private string _startMenuBar= "~Group25 ChatRoom~" + "\r\n" + "1- Register" + "\r\n" + "2- Login" + 
                                 "\r\n" + "3- Exit";
-        string _menuBar= "~Group25 ChatRoom~" + "\r\n" + "4- Send Message" + "\r\n" + "5- view" + 
+        private string _menuBar = "~Group25 ChatRoom~" + "\r\n" + "4- Send Message" + "\r\n" + "5- view" + 
                             "\r\n" + "6- logout" + "\r\n";
-        Boolean isStart;
+        private Boolean isStart;
 
         public GUI()
         {
@@ -41,29 +42,119 @@ namespace MileStone1.PresentationLayer
             {
                 case "1":
                     //go to logic layer and register user
-                    Console.WriteLine("register case");
+                    Console.WriteLine("register:");
+                    Console.WriteLine("Enter your group ID");
+              
+                    string strGroupID = Console.ReadLine();
+
+                    int groupId = checkgroupId(strGroupID);
+
+                    if (groupId.Equals(-1))
+                    {
+                        isStart = true;
+                        displayMenuBar();
+                    }
+
+                    Console.WriteLine("Enter your nickname");
+
+                    string nickname = Console.ReadLine();
+
+                    if (ChatRoom.Instance.register(nickname, groupId))
+                    {
+                        Console.WriteLine("Registered succesfully");
+                        isStart = false;
+                        displayMenuBar();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Registered failed. This nickname is already taken.");
+                        while (!ChatRoom.Instance.register(nickname, groupId) && !nickname.Equals("Y"))
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            // TODO: write the error to the logger
+                            Console.WriteLine("Re-enter your nickname. If you want to return to the menu bar press 'Y':");
+                            nickname = Console.ReadLine();
+                        }
+                        if (nickname.Equals("Y"))
+                        {
+                            isStart = true;
+                            displayMenuBar();
+                        }
+
+                    }
                     displayMenuBar();
                     break;
+
                 case "2":
                     Console.WriteLine("login case");
+                    Console.WriteLine("Enter your groupId");
+
+                    strGroupID = Console.ReadLine();
+                    groupId = checkgroupId(strGroupID);
+
+                    if (groupId.Equals(-1))
+                    {
+                        isStart = true;
+                        displayMenuBar();
+                    }
+
+                    Console.WriteLine("Enter your nickname");
+                    nickname= Console.ReadLine();
+
+                    if (ChatRoom.Instance.login(nickname, groupId))
+                    {
+                        Console.WriteLine("Logged In seccessfully");
+                        isStart = false;
+                        displayMenuBar();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Log In failed. This nickname isn't exist.");
+                        while (!ChatRoom.Instance.login(nickname, groupId) && !nickname.Equals("Y"))
+                        {
+                            Console.WriteLine("Invalid Input.");
+                            // TODO: write the error to the logger
+                            Console.WriteLine("Re-enter your nickname. If you want to return to the menu bar press 'Y':");
+                            nickname = Console.ReadLine();
+                        }
+                        if (nickname.Equals("Y"))
+                        {
+                            isStart = true;
+                            displayMenuBar();
+                        }
+
+                    }
+
+                    //find user by nickname 
+                    User us.loggedIn();
                     displayMenuBar();
                     break;
+
                 case "3":
                     Console.WriteLine("Are u sure u want to exit?");
                     if (Console.ReadLine() == "Y")
-                        // here we need to exit
+                    {
+                        // find user 
+                        user.loggedOut();
+                        //Exit chatroom
+                    }
                     displayMenuBar();
                     break;
+
                 case "4":
                     Console.WriteLine("send case");
                     displayMenuBar();
                     break;
+
                 case "5":
                     Console.WriteLine("send case");
                     displayMenuBar();
                     break;
+
                 case "6":
                     Console.WriteLine("logout case");
+                    //find user
+                    user.loggedOut();
                     isStart = true;
                     displayMenuBar();
                     break;
@@ -75,5 +166,20 @@ namespace MileStone1.PresentationLayer
 
         }
 
-    }
+        private int checkgroupId(string strGroupId)
+        {
+            int groupId;
+            while (!int.TryParse(strGroupId, out groupId) && !strGroupId.Equals("Y"))
+            {
+                Console.WriteLine("Invalid Input.");
+                // TODO: write the error to the logger
+                Console.WriteLine("Re-enter your group ID. If you want to return to the menu for yes press 'Y':");
+                strGroupId = Console.ReadLine();
+            }
+            if (strGroupId.Equals("Y"))
+            {
+                return -1;
+            }
+            return groupId;
+        }
 }
