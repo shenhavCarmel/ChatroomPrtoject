57761e560916773b6299584289298f61f0258ee8
unknown <nofarcar@post.bgu.ac.il>
2018-04-12 13:28:08 +0300

    Final Commit - project finished

--- MileStone1/LogicLayer/ChatRoom.cs
+++ MileStone1/LogicLayer/ChatRoom.cs
@@ -11,6 +11,9 @@ namespace MileStone1.LogicLayer
     // class is defined by singleton design pattern
     class ChatRoom
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+                            (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         // fields
         private static ChatRoom _instance = null;
         private User _loggedInUser;
@@ -27,10 +30,10 @@ namespace MileStone1.LogicLayer
             _userHandler = new UserHandler();
 
             // load users' and messages' data from files
-            //_messages = _msgHandler.GetMessagesList();
-            _messages = new List<Message>();
-            //_registeredUsers = _msgHandler.GetUsersList();
-            _registeredUsers = new List<User>();
+            _messages = _msgHandler.GetMessagesList().ToList();
+            MsgSorter(this._messages);
+
+            _registeredUsers = _userHandler.GetUsersList().ToList();
 
             _loggedInUser = null;
         }
@@ -60,10 +63,18 @@ namespace MileStone1.LogicLayer
                 // connect him to the chat room
                 this._loggedInUser = userToLogin;
 
+                // logger
+                log.Info("User logged in to the chatroom");
                 return true;
             }
             else
+            {
+                // logger
+                log.Error("A non registered user tried to login");
+
                 return false;
+            }
+                
         }
 
         public Boolean Register(String nickname, string groupId)
@@ -75,12 +86,20 @@ namespace MileStone1.LogicLayer
                 _registeredUsers.Add(userToRegister);
 
                 // register him to the chat room --> add to files
-                //_userHandler.SaveUser(userToRegister);
+                _userHandler.SaveUser(userToRegister);
 
+                // logger
+                log.Info("A new user registered");
                 return true;
             }
             else
+            {
+                // logger
+                log.Error("An existing user tried to register again");
+
                 return false;
+            }
+                
         }
 
         private Boolean CheckIfUserExists(User user)
@@ -95,33 +114,63 @@ namespace MileStone1.LogicLayer
             return false;
         }
 
-        public void RetrieveMsg()
+        public Boolean RetrieveMsg()
         {
-            List<IMessage> retrievedMsg = Communication.Instance.GetTenMessages(_URL);
+            try
+            {
+                List<IMessage> retrievedMsg = Communication.Instance.GetTenMessages(_URL);
 
+                // add new messages from the server to the list of messages
+                foreach (IMessage currMsg in retrievedMsg)
+                {
+                    // check if the list of messages already contains message with the same guid
+                    if (!_messages.Exists(e => e.GetId().Equals(currMsg.Id)))
+                    {
+                        _messages.Add(new Message(currMsg));
+                    }
+                }
+
+                // sort the messages list 
+                MsgSorter(this._messages);
+
+                // add to presistent
+                _msgHandler.SaveMessageList(_messages);
+
+                // logger
+                log.Info("Chatroom retrieved 10 messages from server");
+
+                return true;
+            }
+            catch
+            {
+                // logger
+                log.Error("Retrieval messages from sercer failed");
+
+                return false;
+            }
+            
+        }
+
+        private void MsgSorter(List<Message> msgList)
+        {
             // sort the new messages
-            IMessage[] arrRetrievedMsg = retrievedMsg.ToArray();
-            Array.Sort(arrRetrievedMsg, delegate (IMessage msg1, IMessage msg2) {
-                return msg1.Date.CompareTo(msg2.Date);
+            Message[] arrMsg = msgList.ToArray();
+            Array.Sort(arrMsg, delegate (Message msg1, Message msg2) {
+                return msg1.GetDate().CompareTo(msg2.GetDate());
             });
 
-            // add new messages from the server to the list of messages
-            foreach (IMessage currMsg in arrRetrievedMsg)
-            {
-                // check if the list of messages already contains message with the same guid
-                if(!_messages.Exists(e => e.GetId().Equals(currMsg.Id)))
-                {
-                    _messages.Add(new Message(currMsg));
-                }          
-            }
+            // logger
+            log.Info("Quick Messages List was sorted");
 
-            // add to presistent
-            //_msgHandler.SaveMessageList(_messages);
+            msgList = arrMsg.ToList();
         }
 
         public List<Message> DisplayLastMsg()
         {
             const int _DISPLAYED_MSG_AMOUNT = 20;
+
+            // logger
+            log.Info("User displaying 20 last messages");
 
             // check the amount of messages
             if (_messages.Count() < _DISPLAYED_MSG_AMOUNT)
@@ -132,7 +181,7 @@ namespace MileStone1.LogicLayer
             {
                 int startIndex = _messages.Count() - _DISPLAYED_MSG_AMOUNT;
                 return _messages.GetRange(startIndex, _DISPLAYED_MSG_AMOUNT);
-            }
+            }          
         }
 
         public List<Message> DisplayMsgByUser(string nickname, string groupId)
@@ -141,34 +190,52 @@ namespace MileStone1.LogicLayer
 
             if (CheckIfUserExists(sender))
             {
+                // logger
+                log.Info("User displaying messages sent by a specific user");
+
                 // collect messages from a specific user
                 IEnumerable<Message> results =
-                     _messages.Where(currMsg => (currMsg.GetGroupID()).Equals(sender.GetGroupId()) &&
-                                                (currMsg.GetUserName()).Equals(sender.GetNickname()));
-                return (List<Message>)results;
+                     _messages.Where(currMsg => (currMsg.GetGroupID()).Equals(groupId) &&
+                                                (currMsg.GetUserName()).Equals(nickname));
+
+                // into list
+                List<Message> msgFromUser = new List<Message>();
+                foreach (Message currMsg in results)
+                {
+                    msgFromUser.Add(currMsg);
+                }
+                return msgFromUser;
             }
             else
-                throw new ArgumentException("Error - user isn't registered to chatroom");
+            {
+                // logger
+                log.Error("The user tried to display messages from a non-registered user");
 
+                throw new ArgumentException("Error - user isn't registered to chatroom");
+            }
         }
 
-        public string SendMessage(String body)
+        public Boolean SendMessage(String body)
         {
             try
             {
                 // send message content to server through user, save the returned object message
                 Message newSentMessage = _loggedInUser.SendMessage(body, _URL);
 
-                _messages.Add(newSentMessage);
-
                 // save to persistent 
                 _msgHandler.SaveMessage(newSentMessage);
 
-                return "Message sent successfully";
+                // logger
+                log.Info("User sent a new message");
+
+                return true;
             }
-            catch (Exception excep)
+            catch
             {
-                return excep.Message;
+                // logger
+                log.Error("User tried to send a message, sending process failed");
+
+                return false;
             }
         }
 
@@ -179,6 +246,9 @@ namespace MileStone1.LogicLayer
 
             // disconnect him from the chatroom
             _loggedInUser = null;
+
+            // logger
+            log.Info("User logged out");
         }
 
         // check if the end-user can exit (if he loged out first)
--- MileStone1/Start.cs
+++ MileStone1/Start.cs
@@ -1,4 +1,4 @@
-﻿using MileStone1.CommunicationLayer;
+﻿using MileStoneClient.CommunicationLayer;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,31 +11,9 @@ namespace MileStone1
     class Start
     {
         static void Main(string[] args)
-        {
+        {   
             PresentationLayer.GUI start = new PresentationLayer.GUI();
-            string gourpID = "2";
-            string nickName = "Ben";
-            string messageContent = "BenRh safd";
-            string url = "http://ise172.ise.bgu.ac.il";  // url: ip + port
-
-
-            IMessage msg = Communication.Instance.Send(url, gourpID, nickName, messageContent);
-
-            // return with updated time and guid
-            Console.WriteLine("MessageTime:{0} , Guid:{1}\n", msg.Date.ToString(), msg.Id);
-
-            Console.WriteLine(msg + "\n");
-            //Cannot create instance of CommunicationMessage
-            //IMessage msg2 = new CommunicationMessage(); Error
-
-            List<IMessage> msgList = Communication.Instance.GetTenMessages(url);
-            Console.WriteLine("Reuest 10 Last Messages:");
-            foreach (IMessage msgItem in msgList)
-            {
-                Console.WriteLine(msgItem);
-                Console.WriteLine("");
-            }
-            Console.ReadKey();
+           
         }
     }
 }
